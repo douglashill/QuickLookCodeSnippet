d91a2a0b20adea40a328414573adf6b06791f8b0
Douglas Hill <hello@douglashill.co>
2014-01-17 00:00:00 +0000

    Use template for preview. Implementation is now complete, although basic.

--- Source/GeneratePreviewForURL.m
+++ Source/GeneratePreviewForURL.m
@@ -15,8 +15,25 @@ void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview);
 
 OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options)
 {
-	NSData *htmlData = [@"<p>hello</p>" dataUsingEncoding:NSUTF8StringEncoding];
+	NSDictionary *snippet = [NSDictionary dictionaryWithContentsOfURL:(__bridge NSURL *)(url)];
+	NSError *templateReadError;
+	NSString *template = [NSString stringWithContentsOfURL:[[NSBundle bundleWithIdentifier:@"co.douglashill.QuickLookCodeSnippet"] URLForResource:@"template" withExtension:@"html"]
+												  encoding:NSUTF8StringEncoding
+													 error:&templateReadError];
+	if (template == nil) {
+		NSLog(@"Error reading template file: %@", templateReadError);
+		// Should return an error
+	}
+	
+	NSString *htmlString = [NSString stringWithFormat:template,
+							snippet[QLCSTitleKey],
+							snippet[QLCSSummaryKey],
+							snippet[QLCSCompletionPrefixKey],
+							snippet[QLCSContentsKey]];
+	NSData *htmlData = [htmlString dataUsingEncoding:NSUTF8StringEncoding];
+	
 	QLPreviewRequestSetDataRepresentation(preview, (__bridge CFDataRef)(htmlData), kUTTypeHTML, NULL);
+	
 	return noErr;
 }
 
